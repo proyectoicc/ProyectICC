00ff1af05c4eb410826fd2d126239ac1ad8684a6
anth24 <40000508+anth24@users.noreply.github.com>
2018-06-19 12:17:03 -0500

    Código modificado parte 1

--- Proyectofinal.py
+++ Proyectofinal.py
@@ -1,39 +1,37 @@
 p = str(input("Ingrese el producto: "))
-new_cantidad=float(input("Ingrese la cantidad: "))
+cantidad_compra=float(input("Ingrese la cantidad: "))
 clave=str(input("Ingrese clave de comprador: "))
 def pu():
     if p == "P1":
-        return 234
+        return 23.4
     elif p == "P2":
-        return 265
+        return 26.5
     if p == "P3":
-        return 278
+        return 27.8
     elif p == "P4":
-        return 299
+        return 29.9
     if p == "P5":
-        return 334
+        return 33.4
     elif p == "P6":
-        return 365
+        return 36.5
     else:
         print("Error")
-def dsct(new_cantidad,p):
-    if new_cantidad >= 1000:
-        return new_cantidad*0.90*pu()
-    elif new_cantidad >= 100 and new_cantidad < 1000:
-        return new_cantidad*0.93*pu()
-    elif new_cantidad >= 50 and new_cantidad < 100:
-        return new_cantidad * 0.98*pu()
-    elif new_cantidad > 0 and new_cantidad < 50:
-        return new_cantidad*pu()
+def pfinal(cantidad_compra,p):
+    if cantidad_compra >= 1000:
+        return cantidad_compra*0.90*pu()
+    elif cantidad_compra >= 100 and cantidad_compra < 1000:
+        return cantidad*0.93*pu()
+    elif cantidad >= 50 and cantidad < 100:
+        return cantidad * 0.98*pu()
+    elif cantidad > 0 and cantidad < 50:
+        return cantidad*pu()
 def obsequio(clave):
     if clave == "CF1":
-        return 50
+        return 5
     elif clave == "CF2":
-        return 30
+        return 3
     elif clave == "CF3":
-        return 10
+        return 1
     else:
         return 0
-print("El precio final es de: ", dsct(new_cantidad, p), " y su obsequio es :", obsequio)
-
-
+print("El precio final es de: ", pfinal(new_cantidad, p), " y su obsequio es :", obsequio)
